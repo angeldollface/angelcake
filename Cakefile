77a78df44ce576519e78cafe2c86045549cbc186
angeldollface <angeldollface666@gmail.com>
2022-09-12 10:44:38 +0200

    Init.

---
+++ Cakefile
@@ -0,0 +1,2 @@
+% This is a comment.
+'greet' => 'echo Hello World!'
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+/*
+Angel Cake by Alexander Abraham a.k.a. "Angel Dollface".
+Licensed under the MIT license.
+*/
+
+use angelcake::cli;
+
+/// Main entry point
+/// for the Rust compiler.
+fn main(){
+    cli();
+}
